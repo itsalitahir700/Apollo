c8119eccac70fd4d4c4112138d54392023a81a88
Jahanzeb Awan <iamjahanzebawan@gmail.com>
2021-06-01 11:23:12 +0500

    add postrta service

--- src/services/Rta.js
+++ src/services/Rta.js
@@ -1,6 +1,5 @@
 import { baseURL } from "../Config";
 import axios from "axios";
-import { toast } from "react-toastify";
 
 export const getRta = async (token) => {
     try {
@@ -12,23 +11,5 @@ export const getRta = async (token) => {
         return response?.data?.data;
     } catch (error) {
         console.log(error.message);
-    }
-};
-
-export const postRta = async (data, token) => {
-    const response = await axios({
-        method: "post",
-        url: `${baseURL}rta/addNewRtaCase`,
-        data: data,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: token,
-        },
-    });
-    if (response.status === 200) {
-        toast.success(response.data.messages);
-        return response;
-    } else {
-        return false;
     }
 };

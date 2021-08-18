ba1e39afeccbb7df87f6ab354d977ef56ec8c8af
Ali Tahir <alitahir@Alis-MacBook-Air.local>
2021-08-18 17:17:46 +0500

    esign pdf

--- src/pages/ESign/index.js
+++ src/pages/ESign/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import SignaturePad from "react-signature-canvas";
 import { Button } from "primereact/button";
 import styles from "./style.module.css";
@@ -6,16 +6,19 @@ import { addESign, getESigns } from "../../redux/actions/eSignAction";
 import { useDispatch, useSelector } from "react-redux";
 import { InputText } from "primereact/inputtext";
 import { toast } from "react-toastify";
+import { FaFilePdf } from "react-icons/fa";
 
 const ESign = () => {
     let sigPad = {};
     const [loading, setloading] = useState(false);
     const [eDetails, setEDetails] = useState({});
+    const [pdfBase64, setPDFBase64] = useState();
     const eFields = useSelector((state) => state?.eSignSlice?.eFields);
     const dispatch = useDispatch();
     const url = require("url");
     const urlObj = url.parse(document.location.href, true);
     const rtaCode = urlObj?.query?.rta;
+    const pdfRef = useRef(null);
 
     const handleESigns = useCallback(async () => {
         setloading(true);
@@ -44,7 +47,7 @@ const ESign = () => {
         let flag = true;
 
         for (var i in eDetails) {
-            if (eDetails[i] === undefined || eDetails[i] === "") {
+            if ((eDetails[i] === undefined || eDetails[i] === "") && [eDetails[i]] === "doc") {
                 flag = false;
                 break;
             }
@@ -56,27 +59,52 @@ const ESign = () => {
         }
         setloading(false);
     };
-
-    console.log(eDetails);
+    const executeScroll = () => pdfRef.current.scrollIntoView({ top: "400px", behavior: "smooth" });
 
     return (
         <div className={styles.container}>
+            <center>
+                <h2>E-SIGNATURE</h2>
+                <h6>
+                    <b>Please add you signature below</b>{" "}
+                </h6>
+            </center>
             <div className={styles.formContainer}>
                 <div className=" p-fluid p-formgrid p-grid p-my-0">
                     {Object.keys(eFields).map((item) => (
-                        <div className="p-field p-col-12 p-md-6">
-                            <label>{eFields[item]}</label>
-                            <InputText
-                                disabled={loading}
-                                required={true}
-                                type={eFields[item]}
-                                value={eDetails[item]}
-                                onChange={(e) => {
-                                    setEDetails({ ...eDetails, [item]: e.target.value });
-                                }}
-                                placeholder={eFields[item]}
-                            />
-                        </div>
+                        <>
+                            {item === "doc" ? (
+                                <div className="p-md-1 p-sm-2 p-xs-2">
+                                    <Button
+                                        title="Double click to view"
+                                        onClick={() => {
+                                            setPDFBase64(eFields[item]);
+                                        }}
+                                        onDoubleClick={() => {
+                                            setPDFBase64(eFields[item]);
+                                            executeScroll();
+                                        }}
+                                    >
+                                        <FaFilePdf title="Double click to view" className="pdf-file" />
+                                        &nbsp;PDF
+                                    </Button>
+                                </div>
+                            ) : (
+                                <div className="p-field p-col-12 p-md-6">
+                                    <label>{eFields[item]}</label>
+                                    <InputText
+                                        disabled={loading}
+                                        required={true}
+                                        type={eFields[item]}
+                                        value={eDetails[item]}
+                                        onChange={(e) => {
+                                            setEDetails({ ...eDetails, [item]: e.target.value });
+                                        }}
+                                        placeholder={eFields[item]}
+                                    />
+                                </div>
+                            )}
+                        </>
                     ))}
                 </div>
             </div>
@@ -92,6 +120,9 @@ const ESign = () => {
             <div className={styles.buttons}>
                 <Button label="Clear" disabled={loading} className="p-button-secondary" onClick={clear} />
                 <Button label="Submit" disabled={loading} className="p-ml-2" onClick={trim} />
+            </div>
+            <div ref={pdfRef} className="pdf-section p-pb-2">
+                {pdfBase64 ? <embed className="p-mt-4" width="100%" style={{ height: "90vh" }} src={`data:application/pdf;base64,${pdfBase64}`} type="application/pdf" /> : null}
             </div>
         </div>
     );
